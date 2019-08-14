c0aaf8729f7bd8053d145e64134fa29398ac6f4d
Arthur Rubchenko <2146596@mail.ru>
2019-08-14 22:38:39 +0200

    feat: add app build

---
+++ precache-manifest.3670869d2a3101a6a0cc71fed89a5c04.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1fc7b42b95594050e2e9c72cd26b3e9",
+    "url": "/chat-app/index.html"
+  },
+  {
+    "revision": "c080d10f9760f7628d4f",
+    "url": "/chat-app/static/js/2.8163c2a8.chunk.js"
+  },
+  {
+    "revision": "ea2906bb80dbfe96ab69",
+    "url": "/chat-app/static/js/main.0763804e.chunk.js"
+  },
+  {
+    "revision": "1a18b42932e84b2e3ac6",
+    "url": "/chat-app/static/js/runtime~main.1d3039d3.js"
+  }
+]);
